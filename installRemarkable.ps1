46c196e043f074118e29dd6cbe3b2d3dde7c2e96
Jonathan Costin <jonathancostin2@gmail.com>
2024-10-18 10:02:44 -0500

    cleaned up added docs

--- installRemarkable.ps1
+++ installRemarkable.ps1
@@ -1,3 +1,11 @@
+# This script downloads and installs the reMarkable software silently.
+# It performs the following steps:
+# 1. Defines variables for the download URL, destination path, and installation arguments.
+# 2. Downloads the reMarkable installer from the specified URL.
+# 3. Starts the installation process with silent and no-restart options.
+# 4. Checks the exit code of the installation process to determine success or failure.
+# 5. Cleans up by removing the installer file after installation.
+
 # Variables
 $downloadUrl = "https://downloads.remarkable.com/latest/windows"
 $destinationPath = "$env:TEMP\reMarkableInstaller.exe"
@@ -5,13 +13,11 @@ $arguments = "/silent /norestart"
 
 # Download remarkable installer
 Write-Host "Downloading reMarkable installer from $downloadUrl" -ForegroundColor Cyan
-(New-Object Net.WebClient).DownloadFile("$downloadUrl", "$destinationPath")
-
-# Check if the download was successful
-if (Test-Path $destinationPath)
+try
 {
+    (New-Object Net.WebClient).DownloadFile($downloadUrl, $destinationPath)
   Write-Host "Download completed successfully." -ForegroundColor Green
-} else
+} catch
 {
   Write-Host "Download failed! Try again next time." -ForegroundColor Red
   Exit 1
@@ -20,7 +26,7 @@ if (Test-Path $destinationPath)
 # Start the installation process
 Write-Host "Starting the installation of reMarkable..." -ForegroundColor Green
 $process = Start-Process -FilePath $destinationPath -ArgumentList $arguments -PassThru -Wait
-$process.ExitCode
+$exitCode = $process.ExitCode
 
 if ($exitCode -eq 0)
 {
@@ -36,4 +42,3 @@ if (Test-Path $destinationPath)
   Remove-Item $destinationPath -Force
   Write-Host "Installer file has been removed." -ForegroundColor Yellow
 }
-
